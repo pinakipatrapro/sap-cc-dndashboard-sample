f10cfb5764f3a381b70fa9e9aedbc49aec5411ae
pinakipatrapro <pinaki.patra.pro@gmail.com>
2022-07-05 07:26:54 +0530

    Image Changes

--- uimodule/webapp/controllerExtension/DashboardFactory.js
+++ uimodule/webapp/controllerExtension/DashboardFactory.js
@@ -51,7 +51,7 @@ sap.ui.define([
                   "width": "{width}",
                   "height": "{height}",
                   "content": new sap.m.Image({
-                      height: "4rem",
+                      height: "3rem",
                       src: "{url}"
                   })
               })
